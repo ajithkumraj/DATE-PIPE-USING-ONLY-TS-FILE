4cc380333a3ae47a690c57c5c85b2075d5c03eaa
ajithkumraj <ajithkumarajak98@gmail.com>
2022-02-28 11:45:16 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-sw5mce
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-sw5mce)
---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { DatePipe } from '@angular/common';
+import { Component , OnInit} from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent implements OnInit {
+  datePipeString : string;
+Date: Date;
+  
+  
+
+
+  constructor(private datePipe: DatePipe) { 
+    this.datePipeString = datePipe.transform(Date.now(),'yyyy-MM-dd');
+    //  this.datePipeString = datePipe.transform(Date.now(), 'hh:mm:ss'); 
+// this.datePipeString = datePipe.transform(Date.now(),'hh:mm:ss','UTC' );
+//  this.datePipeString = datePipe.transform(Date.now(),'hh:mm:ss','MST' ); 
+
+
+ }  
+
+  onclick(){
+    console.log(this.datePipeString);
+  }
+  ngOnInit() {
+  }
+
+
+}
